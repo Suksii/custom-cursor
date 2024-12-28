ccd4f97e0537cdd7bff06f24ed1a5420edf9da71
unknown <ramovic225@gmail.com>
2024-12-28 14:23:52 +0100

    cursor button created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
-import { motion, transform } from "framer-motion";
 import "./App.css";
 
 const App = () => {
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
   const [isTextHovered, setIsTextHovered] = useState(false);
+  const [isButtonHovered, setIsButtonHovered] = useState(false);
+  const [isButtonClicked, setIsButtonClicked] = useState(false);
+  const [isTextSectionHovered, setIsTextSectionHovered] = useState(false);
+  const [isButtonSectionHovered, setIsButtonSectionHovered] = useState(false);
 
   useEffect(() => {
     const mouseMove = (e) => {
@@ -18,31 +21,67 @@ const App = () => {
 
   const handleMouseTextEnter = () => setIsTextHovered(true);
   const handleMouseTextLeave = () => setIsTextHovered(false);
+  const handleMouseTextSectionEnter = () => setIsTextSectionHovered(true);
+  const handleMouseTextSectionLeave = () => setIsTextSectionHovered(false);
+  const handleMouseButtonSectionEnter = () => setIsButtonSectionHovered(true);
+  const handleMouseButtonSectionLeave = () => setIsButtonSectionHovered(false);
+
+  const handleButtonClick = () => {
+    setIsButtonClicked(true);
+
+    setTimeout(() => setIsButtonClicked(false), 1000);
+  };
 
   return (
     <div className="container">
-      <h1
-        className="text"
-        onMouseEnter={handleMouseTextEnter}
-        onMouseLeave={handleMouseTextLeave}
-      >
-        Hover over this text!
-      </h1>
-
-      <button className="">Click on me!</button>
       <div
-        className="custom-cursor"
-        style={{
-          width: isTextHovered ? "100px" : "40px",
-          height: isTextHovered ? "100px" : "40px",
-          mixBlendMode: "difference",
-          backgroundColor: "#ccc",
-          left: `${mousePosition.x}px`,
-          top: `${mousePosition.y}px`,
-          transform: "translate(-50%, -50%)",
-          transition: "width 0.3s, height 0.3s, background-color 0.3s"
-        }}
-      />
+        className="text-section"
+        style={{ flex: 1 }}
+        onMouseEnter={handleMouseTextSectionEnter}
+        onMouseLeave={handleMouseTextSectionLeave}
+      >
+        <h1
+          className="text"
+          onMouseEnter={handleMouseTextEnter}
+          onMouseLeave={handleMouseTextLeave}
+        >
+          Hover over this text!
+        </h1>
+
+        {isTextSectionHovered && (
+          <div
+            className="custom-cursor-text"
+            style={{
+              width: isTextHovered ? "100px" : "40px",
+              height: isTextHovered ? "100px" : "40px",
+              mixBlendMode: "difference",
+              left: `${mousePosition.x}px`,
+              top: `${mousePosition.y}px`,
+              transform: "translate(-50%, -50%)",
+              transition: "width 0.3s, height 0.3s, background-color 0.3s",
+            }}
+          />
+        )}
+        {isButtonSectionHovered && (
+          <div
+            className="custom-cursor-button"
+            style={{
+              width: isTextHovered ? "100px" : "40px",
+              height: isTextHovered ? "100px" : "40px",
+              left: `${mousePosition.x}px`,
+              top: `${mousePosition.y}px`,
+              transition: "width 0.3s, height 0.3s, background-color 0.3s",
+            }}
+          />
+        )}
+      </div>
+      <div
+        className="button-section"
+        onMouseEnter={handleMouseButtonSectionEnter}
+        onMouseLeave={handleMouseButtonSectionLeave}
+      >
+        <button onClick={handleButtonClick}>Click on me!</button>
+      </div>
     </div>
   );
 };
